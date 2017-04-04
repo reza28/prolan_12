e73bdf31dea3d568662fdc6f2774bf6b30edbd08
reza28 <granesha.reza@gmail.com>
2017-04-04 13:18:43 +0700

    tipe data

---
+++ TipeData/src/tipedata/TipeData.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipedata;
+
+/**
+ *
+ * @author reza
+ */public class TipeData{
+  public static void main(String[] args){
+   // tipe data objek
+   String nama="Belajar Java yuks";
+   System.out.println(nama.length());
+   
+   //tipe data byte
+   byte makByte=127;
+   byte minByte=-127;
+   byte defaultByte=0;
+   System.out.println(defaultByte);
+   
+   //tipe data short
+   short maxShort=32767;
+   short minShort=-32767;
+   short defaultShort=0;
+   System.out.println(defaultShort);
+   
+   //tipe data int
+   int maxInt =214783648;
+   int minInt =-214783648;
+   int defaultInt=0;
+   System.out.println(defaultInt);
+   
+   //tipe data long
+   long maxLong=9223372036854775807L;
+   long minLong=-922337203685477580L;
+   long defaultLong=0L;
+   System.out.println(defaultLong);
+   
+   //tipe data float
+   float maxFloat=3.40282347999999999999999999999999999999999999f;
+   float minFloat=-3.40282347999999999999999999999999999999999999f;
+   float defaultFloat=0.0f;
+   System.out.println(maxFloat);
+   System.out.println(minFloat);
+   
+   //tipe data double
+   double randomDouble=1.797693134862315702d;
+   double randomDouble1=-1.797693134862315702d;
+   double defaultDouble=0.0;
+   System.out.println(randomDouble);
+   }
+ }
