ae87bb5d896adad2d18cb8ed9da0c16cbf0e5483
reza28 <granesha.reza@gmail.com>
2017-04-04 10:27:11 +0700

    program input
    program input (CMD)

--- HalloWorldCMD/helloworld.java
+++ HalloWorldCMD/helloworld.java
@@ -1,6 +1,12 @@
-/*program hello world*/
-class HalloWorld {
-		public static void main(String argv[]){
-		System.out.println("Hello World");
+/*program ke-3*/
+public class InputMahasiswa
+{
+public static void main(String []args){
+	Scanner InputMahasiswa = new Scanner (System.in);
+	String mahasiswa;
+	
+	mahasiswa = input.nextLine();
+	
+	System.out.println(mahasiswa)
 	}
 }
---
+++ InputMahasiswa/InputMahasiswa.java
@@ -0,0 +1,13 @@
+/*program ke-3*/
+import java.util.Scanner;
+public class InputMahasiswa
+{
+public static void main(String []args){
+	Scanner InputMahasiswa = new Scanner(System.in);
+	String mahasiswa;
+	
+	mahasiswa = InputMahasiswa.nextLine();
+	
+	System.out.println(mahasiswa);
+	}
+}
